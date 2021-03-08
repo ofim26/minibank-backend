acb38cc06a55657579eee4190fec9d4de0ffa2df
Felipe Idárraga <felipe.idarraga@tutenlabs.com>
2021-03-08 13:00:15 -0300

    minibank project

--- src/config/dbconfig.ts
+++ src/config/dbconfig.ts
@@ -1,8 +1,8 @@
 export const dbConfig = {
-    HOST: "localhost",
-    USER: "adminbank",
-    PASSWORD: "a1s2d3f4",
-    DB: "minibank",
+    HOST: "ec2-52-7-115-250.compute-1.amazonaws.com",
+    USER: "coxhvhvaihmfsb",
+    PASSWORD: "702e92618acde88b26b5a60756cd0ed959e8bc8449bb51393909d074dac5d091",
+    DB: "dbj6arm6pkml4o",
     PORT: 5432,
     dialect: "postgres",
     pool: {
