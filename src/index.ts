98bc3cc13de7e3097e373f96da7786243973747a
Felipe Idárraga <felipe.idarraga@tutenlabs.com>
2021-03-09 07:59:30 -0300

    minibank project

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import bodyParser from "body-parser";
+import cors from "cors";
 import express from "express";
-import cors  from "cors";
 import swaggerUi from "swagger-ui-express";
 import { sequelize } from "./config/sequelize";
 import { getSwaggerSpecs } from "./config/swagger";
@@ -13,7 +13,7 @@ import { usersRoutes } from "./Routes/UsersRoutes";
 const app = express();
 
 // CORS
-app.use(cors())
+app.use(cors());
 app.use( (req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
